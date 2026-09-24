4686f3c457f8a66340ac3309db65b6972c15b039
osman037 <usmanrehmaniofficial@GMAIL.COM>
2026-09-24 12:35:55 +0500

    chore: root for CybersecurityNetworkMap

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import { Composition } from "remotion";
 import { CybersecurityNetworkMap } from "./composition";
 
-export const RemotionRoot: React.FC = () => {
-  return (
-    <>
-      <Composition
-        id="CybersecurityNetworkMap"
-        component={CybersecurityNetworkMap}
-        width={3840}
-        height={2160}
-        fps={60}
-        durationInFrames={900}
-      />
-    </>
-  );
-};
+export const RemotionRoot: React.FC = () => (
+  <>
+    <Composition
+      id="CybersecurityNetworkMap"
+      component={CybersecurityNetworkMap}
+      width={3840}
+      height={2160}
+      fps={60}
+      durationInFrames={900}
+    />
+  </>
+);
