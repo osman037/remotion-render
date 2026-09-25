3bb73d31b33e83d9d8a5970bd7106144da22a899
osman037 <usmanrehmaniofficial@GMAIL.COM>
2026-09-25 07:25:56 +0500

    chore: bulk root (2 comps)

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { Composition } from "remotion";
-import { CybersecurityNetworkMap } from "./composition";
+import { CybersecurityNetworkMap } from "./compositions/comp0";
+import { CybersecurityNetworkMap } from "./compositions/comp1";
 
 export const RemotionRoot: React.FC = () => (
   <>
-    <Composition
-      id="CybersecurityNetworkMap"
-      component={CybersecurityNetworkMap}
-      width={3840}
-      height={2160}
-      fps={60}
-      durationInFrames={900}
-    />
+    <Composition id="CybersecurityNetworkMap" component={CybersecurityNetworkMap} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="CybersecurityNetworkMap" component={CybersecurityNetworkMap} width={3840} height={2160} fps={60} durationInFrames={900} />
   </>
 );
