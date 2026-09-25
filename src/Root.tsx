71ff2d161143a391750737673abc790a9074bb61
osman037 <usmanrehmaniofficial@GMAIL.COM>
2026-09-26 02:07:42 +0500

    chore: bulk root (2 comps)

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { Composition } from "remotion";
-import { AdobeStockTrajectory } from "./composition";
+import { CybersecurityNetworkMap } from "./compositions/comp0";
+import { AIDashboardOverlay } from "./compositions/comp1";
 
 export const RemotionRoot: React.FC = () => (
   <>
-    <Composition
-      id="AdobeStockTrajectory"
-      component={AdobeStockTrajectory}
-      width={3840}
-      height={2160}
-      fps={60}
-      durationInFrames={900}
-    />
+    <Composition id="CybersecurityNetworkMap" component={CybersecurityNetworkMap} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="AIDashboardOverlay" component={AIDashboardOverlay} width={3840} height={2160} fps={60} durationInFrames={900} />
   </>
 );
