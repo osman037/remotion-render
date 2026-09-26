643089296ea6cea539dd7add6be3494fd965c1cc
osman037 <usmanrehmaniofficial@GMAIL.COM>
2026-09-26 09:26:02 +0500

    chore: bulk root (12 comps)

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,31 +1,31 @@
 import React from "react";
 import { Composition } from "remotion";
-import { BiometricFaceScan } from "./compositions/comp0";
-import { CICDPipelineDeploy } from "./compositions/comp1";
-import { CandlestickTickerTape } from "./compositions/comp2";
-import { CarbonEmissionsDashboard } from "./compositions/comp3";
-import { ContourRingsLoop } from "./compositions/comp4";
-import { CorporateLowerThirds } from "./compositions/comp5";
-import { EKGMedicalMonitor } from "./compositions/comp6";
-import { EcommerceBarRace } from "./compositions/comp7";
-import { GPUTrainingMonitor } from "./compositions/comp8";
-import { GlitchTransitionSampler } from "./compositions/comp9";
-import { GrowthMindsetKineticType } from "./compositions/comp10";
-import { NeonCountdownRing } from "./compositions/comp11";
+import { CSRDMaterialityMatrix } from "./compositions/comp0";
+import { CBAMCertificateFlow } from "./compositions/comp1";
+import { AIAgentOrchestration } from "./compositions/comp2";
+import { EIAIActCompliance } from "./compositions/comp3";
+import { NIS2IncidentReporting } from "./compositions/comp4";
+import { ZeroTrustAccessFlow } from "./compositions/comp5";
+import { StablecoinPaymentRail } from "./compositions/comp6";
+import { RAGPipelineFlow } from "./compositions/comp7";
+import { FinOpsCloudDashboard } from "./compositions/comp8";
+import { RemotePatientMonitoring } from "./compositions/comp9";
+import { DistributedTracingWaterfall } from "./compositions/comp10";
+import { MFAExpiryTimer } from "./compositions/comp11";
 
 export const RemotionRoot: React.FC = () => (
   <>
-    <Composition id="BiometricFaceScan" component={BiometricFaceScan} width={1080} height={1920} fps={60} durationInFrames={1200} />
-    <Composition id="CICDPipelineDeploy" component={CICDPipelineDeploy} width={1080} height={1920} fps={60} durationInFrames={1200} />
-    <Composition id="CandlestickTickerTape" component={CandlestickTickerTape} width={1080} height={1920} fps={60} durationInFrames={1200} />
-    <Composition id="CarbonEmissionsDashboard" component={CarbonEmissionsDashboard} width={1080} height={1920} fps={60} durationInFrames={1200} />
-    <Composition id="ContourRingsLoop" component={ContourRingsLoop} width={1080} height={1920} fps={60} durationInFrames={1200} />
-    <Composition id="CorporateLowerThirds" component={CorporateLowerThirds} width={1080} height={1920} fps={60} durationInFrames={1200} />
-    <Composition id="EKGMedicalMonitor" component={EKGMedicalMonitor} width={1080} height={1920} fps={60} durationInFrames={1200} />
-    <Composition id="EcommerceBarRace" component={EcommerceBarRace} width={1080} height={1920} fps={60} durationInFrames={1200} />
-    <Composition id="GPUTrainingMonitor" component={GPUTrainingMonitor} width={1080} height={1920} fps={60} durationInFrames={1200} />
-    <Composition id="GlitchTransitionSampler" component={GlitchTransitionSampler} width={1080} height={1920} fps={60} durationInFrames={1200} />
-    <Composition id="GrowthMindsetKineticType" component={GrowthMindsetKineticType} width={1080} height={1920} fps={60} durationInFrames={1200} />
-    <Composition id="NeonCountdownRing" component={NeonCountdownRing} width={1080} height={1920} fps={60} durationInFrames={1200} />
+    <Composition id="CSRDMaterialityMatrix" component={CSRDMaterialityMatrix} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="CBAMCertificateFlow" component={CBAMCertificateFlow} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="AIAgentOrchestration" component={AIAgentOrchestration} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="EIAIActCompliance" component={EIAIActCompliance} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="NIS2IncidentReporting" component={NIS2IncidentReporting} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="ZeroTrustAccessFlow" component={ZeroTrustAccessFlow} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="StablecoinPaymentRail" component={StablecoinPaymentRail} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="RAGPipelineFlow" component={RAGPipelineFlow} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="FinOpsCloudDashboard" component={FinOpsCloudDashboard} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="RemotePatientMonitoring" component={RemotePatientMonitoring} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="DistributedTracingWaterfall" component={DistributedTracingWaterfall} width={3840} height={2160} fps={60} durationInFrames={900} />
+    <Composition id="MFAExpiryTimer" component={MFAExpiryTimer} width={3840} height={2160} fps={60} durationInFrames={900} />
   </>
 );
